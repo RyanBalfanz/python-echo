75dd1b9efb2f97f70732766ff766e49b9037f4c9
Ryan Balfanz <rbalfanz@netflix.com>
2014-02-21 09:25:59 -0800

    Echo'd query string like @progrium
    	http://progrium.com/blog/2013/01/05/executable-tweets-and-programs-in-short-urls/

--- server.py
+++ server.py
@@ -1,17 +1,24 @@
 import os
+import urllib2
 
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/")
+def echo():
+	data = urllib2.unquote(request.query_string)
+	if ' ' not in data:
+		return data.replace('+', ' ')
+	else:
+		return data
 
 if __name__ == '__main__':
-    app.run(
-    	debug=os.getenv("DEBUG", True),
-    	host="0.0.0.0",
-    	port=int(os.getenv("PORT", 5000))
-    )
+	debug = os.getenv("DEBUG", True)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(
+		debug=debug,
+		host="0.0.0.0",
+		port=port,
+	)
